f21739f0b10ab1e7161d5f3a546ef264e33006f9
Martin DEWAELE <79399364+martindewaele@users.noreply.github.com>
2022-05-16 08:45:19 +0200

    Update poule_house.ino

--- poule_house.ino
+++ poule_house.ino
@@ -1,12 +1,15 @@
+// Déclaration des deux pin connectés vers le pont en H
 int verrin_pin1 = 2;
 int verrin_pin2 = 3;
 
-
+//Déclaration des deux pin en mode output
 void setup() {
-  // put your setup code here, to run once:
   pinMode(verrin_pin1, OUTPUT);
   pinMode(verrin_pin2, OUTPUT);
 }
+
+//Boucle qui permet de lire toutes les 10 minutes la valeur renvoyée par la LDR
+//Si la valeur est inférieure à 300, le verrin se ferme, sinon il s'ouvre.
 
 void loop() {
   int lum = analogRead(A0);
